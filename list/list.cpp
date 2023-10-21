791adac338d4cfd5c55c5ae0282400def24a7cc4
cissy <1787393382@qq.com>
2023-10-21 16:12:30 +0800

    creat list dir

---
+++ list/list.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cissy on 2023/10/21.
+//
+
+#include "list.h"
+
+int list::f(int x) {
+    return x+2;
+}
---
+++ list/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cissy on 2023/10/21.
+//
+#include <iostream>
+#include "list.h"
+
+int main() {
+    std::cout << list::f(2) << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
